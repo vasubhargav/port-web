e680f72785d330d4b4f9961a4644b71cd35e6f2d
Vasubhargav <vasubhargav295@gmail.com>
2026-09-05 16:32:10 +0530

    feat: initial commit for django portfolio website

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,"portfolio/home.html")
+def about(request):
+    return render(request,'portfolio/about.html')
+def contact(request):
+    return render(request,'portfolio/contact.html')
