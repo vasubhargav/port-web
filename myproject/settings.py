e08ba19fc33088df4db5c04a6fedcec37e6ae1bd
Vasubhargav <vasubhargav295@gmail.com>
2026-09-06 17:19:44 +0530

    initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS= [ BASE_DIR /'portfolio' / 'static']
+STATICFILES_DIRS= [ BASE_DIR  / 'static']
+STATIC_ROOT = [BASE_DIR, 'staticfiles']
 
 
 # Email
